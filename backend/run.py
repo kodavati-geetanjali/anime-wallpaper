c18e8b1e34763b2bd3aabf591606af9ad19ff6d6
Kodavati Geetanjali <kodavatigeetanjali@gmail.com>
2025-08-01 18:50:12 +0530

    Adding requirement.txt

--- backend/run.py
+++ backend/run.py
@@ -9,4 +9,6 @@ from app.routes import routes
 app.register_blueprint(routes)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
